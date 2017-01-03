0f138329212c6446d86545bc18a2efe3b55be5d9
hitoshi54 <dylanclement7@gmail.com>
2017-01-03 17:09:40 +0100

    Mise à jour de la bdd

--- lbs.sql
+++ lbs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
+-- version 4.5.4.1
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Mar 06 Décembre 2016 à 16:52
+-- Client :  localhost
+-- Généré le :  Mar 03 Janvier 2017 à 18:09
 -- Version du serveur :  5.7.11
--- Version de PHP :  7.0.4
+-- Version de PHP :  5.6.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -51,9 +51,17 @@ INSERT INTO `categorie` (`id`, `nom`, `description`) VALUES
 
 CREATE TABLE `commande` (
   `id` int(11) NOT NULL,
+  `montant` float NOT NULL,
   `dateretrait` date DEFAULT NULL,
   `etat` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `montant`, `dateretrait`, `etat`) VALUES
+(1, 0, '2017-01-04', 1);
 
 -- --------------------------------------------------------
 
@@ -62,7 +70,7 @@ CREATE TABLE `commande` (
 --
 
 CREATE TABLE `contenir` (
-  `id` int(11) NOT NULL,
+  `id_sandwich` int(11) NOT NULL,
   `id_ingredient` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -94,7 +102,7 @@ INSERT INTO `ingredient` (`id`, `nom`, `cat_id`, `description`, `fournisseur`, `
 (6, 'avocat', 2, 'avocats en direct du Mexique !', 'la huerta bonita, Puebla', NULL),
 (7, 'blanc de poulet', 3, 'blanc de poulet émincé, et grillé comme il faut', 'élevage "le poulet volant"', NULL),
 (8, 'magret de canard', 3, 'magret de canard grillé, puis émincé', 'le colvert malin', NULL),
-(9, 'steack haché', 3, 'notre steack haché saveur, 5% MG., préparé juste avant cuisson.\r\nViande de notre producteur local.', 'ferme "la vache qui plane"', NULL),
+(9, 'steack haché', 3, 'notre steack haché saveur, 5% MG., préparé juste avant cuisson.\nViande de notre producteur local.', 'ferme "la vache qui plane"', NULL),
 (10, 'munster', 4, 'Du munster de Munster, en direct. Pour amateurs avertis !', 'fromagerie "le bon munster de toujours"', NULL),
 (11, 'chèvre frais', 4, 'un chèvre frais onctueux et goutu !', 'A la chèvre rieuse', NULL),
 (12, 'comté AOC 18mois', 4, 'le meilleur comté du monde !', 'fromagerie du jura', NULL),
@@ -135,10 +143,10 @@ ALTER TABLE `commande`
 -- Index pour la table `contenir`
 --
 ALTER TABLE `contenir`
-  ADD PRIMARY KEY (`id`,`id_ingredient`),
+  ADD PRIMARY KEY (`id_sandwich`,`id_ingredient`),
   ADD KEY `FK_contenir_id_ingredient` (`id_ingredient`),
-  ADD KEY `id` (`id`),
-  ADD KEY `id_2` (`id`);
+  ADD KEY `id` (`id_sandwich`),
+  ADD KEY `id_2` (`id_sandwich`);
 
 --
 -- Index pour la table `ingredient`
@@ -167,7 +175,7 @@ ALTER TABLE `categorie`
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `ingredient`
 --
@@ -186,7 +194,7 @@ ALTER TABLE `sandwich`
 -- Contraintes pour la table `contenir`
 --
 ALTER TABLE `contenir`
-  ADD CONSTRAINT `FK_contenir_id` FOREIGN KEY (`id`) REFERENCES `sandwich` (`id`),
+  ADD CONSTRAINT `FK_contenir_id` FOREIGN KEY (`id_sandwich`) REFERENCES `sandwich` (`id`),
   ADD CONSTRAINT `FK_contenir_id_ingredient` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id`);
 
 --
